7e20690b416ebeb27a0653a3ca32ead89755c9fc
Krit Daowaset <kidrunner1@gmail.com>
2023-09-19 01:08:30 +0700

    update admin screen

--- lib/screen/admin_screen.dart
+++ lib/screen/admin_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // Make sure to import this package
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tracker_covid_v1/model/users.dart';
 import 'admin_manager.dart';
 
@@ -29,6 +29,17 @@ class _AdminScreenState extends State<AdminScreen> {
     });
   }
 
+  Stream<List<Users>> _getUsersStream() {
+    return FirebaseFirestore.instance.collection('users').snapshots().map(
+          (snapshot) => snapshot.docs
+              .map(
+                (doc) => Users.fromDocument(
+                    doc), // Use fromDocument instead of fromFirestore
+              )
+              .toList(),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,58 +48,61 @@ class _AdminScreenState extends State<AdminScreen> {
         actions: roleID == 1
             ? [
                 IconButton(
-                    onPressed: () => {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => AdminManagerScreen(),
-                            ),
-                          )
-                        },
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AdminManagerScreen(),
+                        ),
+                      );
+                    },
                     icon: const Icon(Icons.settings))
               ]
             : null,
         backgroundColor: Colors.red[300],
       ),
-      body: FutureBuilder<List<Users>?>(
-        future: Users.getUsers(),
-        builder: (context, AsyncSnapshot<List<Users>?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.data == null) {
-              return const Center(child: Text('Something went wrong'));
-            }
-            List<Users>? users = snapshot.data;
-            return ListView.builder(
-              itemCount: users!.length,
-              itemBuilder: (context, index) {
-                Users u = users[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  elevation: 5.0,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  child: ListTile(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                    title: Text(u.email ?? "No Email",
-                        style: GoogleFonts.prompt(fontSize: 16.0)),
-                    subtitle: Text(
-                      "${u.firstName} ${u.lastName} - ${roleToString(u.role ?? 3)}",
-                      style: GoogleFonts.prompt(
-                          fontSize: 14.0, color: Colors.grey[600]),
-                    ),
-                    trailing: Chip(
-                      label: Text(roleToString(u.role ?? 3),
-                          style: GoogleFonts.prompt(fontSize: 12.0)),
-                      backgroundColor: roleToColor(u.role ?? 3),
-                    ),
-                    onTap: () => _showRoleDialog(u),
-                  ),
-                );
-              },
-            );
+      body: StreamBuilder<List<Users>>(
+        stream: _getUsersStream(),
+        builder: (context, AsyncSnapshot<List<Users>> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
           }
-          return const CircularProgressIndicator();
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+
+          List<Users> users = snapshot.data!;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              Users u = users[index];
+              return Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  title: Text(u.email ?? "No Email",
+                      style: GoogleFonts.prompt(fontSize: 16.0)),
+                  subtitle: Text(
+                    "${u.firstName} ${u.lastName} - ${roleToString(u.role ?? 3)}",
+                    style: GoogleFonts.prompt(
+                        fontSize: 14.0, color: Colors.grey[600]),
+                  ),
+                  trailing: Chip(
+                    label: Text(roleToString(u.role ?? 3),
+                        style: GoogleFonts.prompt(fontSize: 12.0)),
+                    backgroundColor: roleToColor(u.role ?? 3),
+                  ),
+                  onTap: () => _showRoleDialog(u),
+                ),
+              );
+            },
+          );
         },
       ),
     );
@@ -100,30 +114,72 @@ class _AdminScreenState extends State<AdminScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Update User Role'),
-          content: DropdownButton<int>(
-            value: selectedRole,
-            onChanged: (newValue) {
-              setState(() {
-                selectedRole = newValue;
-              });
-            },
-            items: <int>[1, 2, 3].map<DropdownMenuItem<int>>((int value) {
-              return DropdownMenuItem<int>(
-                value: value,
-                child: Text(roleToString(value)),
-              );
-            }).toList(),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: const Text(
+            'กำหนดสิทธิ์ผู้ใช้งาน',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+            ),
+          ),
+          content: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.grey.shade300),
+            ),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<int>(
+                isExpanded: true,
+                value: selectedRole,
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedRole = newValue;
+                  });
+                },
+                items: <int>[1, 2, 3].map<DropdownMenuItem<int>>((int value) {
+                  return DropdownMenuItem<int>(
+                    value: value,
+                    child: Text(
+                      roleToString(value),
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                  );
+                }).toList(),
+                dropdownColor: Colors.white,
+              ),
+            ),
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('CANCEL'),
+              style: TextButton.styleFrom(
+                primary: Colors.grey.shade800,
+              ),
+              child: const Text(
+                'CANCEL',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: Text('UPDATE'),
+              style: TextButton.styleFrom(
+                primary: Colors.blue,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 25.0, vertical: 10.0),
+              ),
+              child: const Text(
+                'UPDATE',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
               onPressed: () async {
                 if (selectedRole != null) {
                   await _updateUserRole(user.uid, selectedRole!);
