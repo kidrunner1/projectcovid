bd485a767f53572fe79d56caaf4f0114bb2fde85
Demithier01 <102402386+Demithier01@users.noreply.github.com>
2023-10-09 12:03:44 +0700

    edit

--- lib/screen/appointment/getdata_appoints.dart
+++ lib/screen/appointment/getdata_appoints.dart
@@ -102,6 +102,7 @@ class _GetdataAppointsState extends State<GetdataAppoints> {
                     ),
                   ),
                 ),
-            ),
+            ]),
           ),
-        ),
+        )));
+  }}
